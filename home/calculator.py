7d79b9502ec1ca54805bcdb07794096b06bedbd4
kubanychbek11 <kuba.seitkaziev@gmail.com>
2022-12-14 18:40:13 +0600

    hw3

---
+++ home/calculator.py
@@ -0,0 +1,13 @@
+class calculate:
+    def __init__(self,value):
+        self.value = value
+    def __add__(self, other):
+        return self.value+other.value
+    def __sub__(self, other):
+        return self.value-other.value
+    def __mul__(self, other):
+        return self.value * other.value
+    def __truediv__(self, other):
+        return self.value / other.value
+
+
